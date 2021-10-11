e405fa998ff97c6c968f02197749d12686efaa80
Aktsent <accent43@yandex.ru>
2021-10-11 16:31:49 +0300

    Error

---
+++ src/main/java/ru/job4j/tracker/oop/Error.java
@@ -0,0 +1,34 @@
+package ru.job4j.tracker.oop;
+
+public class Error {
+
+    private boolean active;
+
+    private int status;
+
+    private String message;
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public Error() {
+    }
+
+    public void printInfo() {
+        System.out.println("Активность: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error(true, 5, "Well Done");
+        Error error1 = new Error();
+        error.printInfo();
+        System.out.println();
+        error1.printInfo();
+    }
+
+}

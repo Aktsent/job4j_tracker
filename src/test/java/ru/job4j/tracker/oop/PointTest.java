c5ce58708ee53bbcab8097ac20f0fc8602e0526a
Aktsent <accent43@yandex.ru>
2021-10-18 15:50:04 +0300

    Point

--- src/test/java/ru/job4j/tracker/oop/PointTest.java
+++ src/test/java/ru/job4j/tracker/oop/PointTest.java
@@ -1,16 +1,27 @@
 package ru.job4j.tracker.oop;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
 import static org.hamcrest.Matchers.closeTo;
 import static org.junit.Assert.assertThat;
 
 public class PointTest extends TestCase {
 
+    @Test
     public void testDistance() {
         Point a = new Point(0, 0);
         Point b = new Point(0, 2);
         double dist = a.distance(b);
         assertEquals(2, dist, 0.01);
+    }
+
+    @Test
+    public void testDistance3d() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        Point c = new Point(0, 0);
+        double dist = a.distance3d(c);
+        assertEquals(0, dist, 0.01);
     }
 }

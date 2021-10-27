ddf498f8784548a6ed8808875e80db2eef158fb4
Aktsent <accent43@yandex.ru>
2021-10-27 16:21:51 +0300

    Book

--- src/main/java/ru/job4j/tracker/pojo/Library.java
+++ src/main/java/ru/job4j/tracker/pojo/Library.java
@@ -2,25 +2,32 @@ package ru.job4j.tracker.pojo;
 
 public class Library {
     public static void main(String[] args) {
-        Book book = new Book();
-        Book book1 = new Book();
-        Book book2 = new Book();
-        Book book3 = new Book();
+        Book book = new Book("Clean code", 5);
+        Book book1 = new Book("Book 1", 6);
+        Book book2 = new Book("Book 2", 7);
+        Book book3 = new Book("Book UP", 8);
         Book[] books = new Book[4];
         books[0] = book;
         books[1] = book1;
         books[2] = book2;
         books[3] = book3;
-        book.setName("Clean code");
         for (int i = 0; i < books.length; i++) {
             Book bks = books[i];
             System.out.println(bks.getName());
-            Book temp = books[0];
-            books[0] = books[3];
-            books[3] = temp;
+        }
+        System.out.println();
+        Book temp = books[3];
+        books[3] = books[0];
+        books[0] = temp;
+        for (int i = 0; i < books.length; i++) {
+            Book bks = books[i];
             System.out.println(bks.getName());
-            if (books[i].equals("Clean code")) {
-                System.out.println(bks.getName());
+        }
+        System.out.println();
+
+        for (int i = 0; i < books.length; i++) {
+            if ("Clean code".equals(book.getName())) {
+                System.out.println(book.getName());
             }
         }
     }

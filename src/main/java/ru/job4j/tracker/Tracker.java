7b2f6e0ed3290ba548a4aa7cc5dbdc7115f735f2
Aktsent <accent43@yandex.ru>
2021-11-02 16:26:16 +0300

    Tracker

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -11,10 +13,34 @@ public class Tracker {
         return item;
     }
 
+    public Item[] findAll() {
+        Item[] item = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+            Item array = items[i];
+            if (array != null) {
+                item[size] = array;
+                size++;
+            }
+        }
+        item = Arrays.copyOf(item, size);
+        return item;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] item = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+            Item array = items[i];
+            if (array.getName().equals(key)) {
+                item[i] = array;
+            }
+        }
+        return item;
+    }
+
     public Item findById(int id) {
         Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
+        for (int i = 0; i < size; i++) {
+            Item item = items[i];
             if (item.getId() == id) {
                 rsl = item;
                 break;

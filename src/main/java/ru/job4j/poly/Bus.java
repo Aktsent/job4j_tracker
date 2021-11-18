b84e5a363d242ed71f3f6fe7de9069767ad1287e
Aktsent <accent43@yandex.ru>
2021-11-18 15:33:50 +0300

    Transport

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+    }
+
+    @Override
+    public void passenger(int number) {
+    }
+
+    @Override
+    public int refuel(int fuel) {
+        int price = 5;
+        return price;
+    }
+}

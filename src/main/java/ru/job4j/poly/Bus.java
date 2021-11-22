2dc6df544388ff668e12517844fc920f21c202f0
Aktsent <accent43@yandex.ru>
2021-11-22 15:51:47 +0300

    Bus

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -4,15 +4,20 @@ public class Bus implements Transport {
 
     @Override
     public void drive() {
+        String driver = "Ivanov";
+        System.out.println(driver);
     }
 
     @Override
     public void passenger(int number) {
+        int seat = 20;
+        int free = number / seat;
+        System.out.println(free);
     }
 
     @Override
     public int refuel(int fuel) {
         int price = 5;
-        return price;
+        return fuel * price;
     }
 }

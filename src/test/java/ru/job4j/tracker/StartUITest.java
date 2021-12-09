0fac93cf10bc341a9202a6e7ca04ba96b413331b
Aktsent <accent43@yandex.ru>
2021-12-09 13:15:10 +0300

    StartUITest

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -21,7 +21,7 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Replaced item"));
         String replacedName = "New item name";
-        Input in = new StubInput(new String[] {"0", "Enter name: ", "1"});
+        Input in = new StubInput(new String[] {"0", "Edit id ", "Edit name", "1"});
         UserAction[] actions = {new EditIthem(), new ExitAction()};
         new StartUI().init(in, tracker, actions);
         assertThat(tracker.findById(item.getId()).getName(), is(replacedName));

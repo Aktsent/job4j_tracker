92f8d6779a17972266e7888a2ba373d282fb0f5c
Aktsent <accent43@yandex.ru>
2021-12-23 10:51:19 +0300

    FindEl

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,22 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i] == null) {
+                throw new ElementNotFoundException("Element nof found");
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] value = {};
+        try {
+            indexOf(value, "3");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}

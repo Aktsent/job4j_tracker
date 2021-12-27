64a16c7269bc01bca0fd4310457cf666771efcad
Aktsent <accent43@yandex.ru>
2021-12-27 11:25:39 +0300

    UserStore

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -2,23 +2,20 @@ package ru.job4j.ex;
 
 public class UserStore {
     public static User findUser(User[] users, String login) throws UserNotFoundException {
-        for (int i = 0; i < users.length; i++) {
-            if (users[i].equals(login)) {
-                return users[i];
+        for (User user : users) {
+            if (user.getUsername().equals(login)) {
+                return user;
             }
-        }
-        if (users == null) {
             throw new UserNotFoundException("User not found");
         }
         return null;
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        user = new User("Name", true);
-        if (user.isValid() && user.getUsername().length() < 3) {
+        if (!user.isValid() || user.getUsername().length() < 3) {
             throw new UserInvalidException("User not valid");
         }
-        return false;
+        return true;
     }
 
     public static void main(String[] arg) {

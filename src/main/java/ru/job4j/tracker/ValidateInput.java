d56b9a5f125d64f5252095074485fbf679be79d3
Aktsent <accent43@yandex.ru>
2022-01-11 15:37:05 +0300

    ValidateInputTest

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -33,12 +33,17 @@ public class ValidateInput implements Input {
     private boolean isNumber(String value) {
         boolean rsl = true;
         char[] check = value.toCharArray();
-        for (char num : check) {
-            if (num < 48 || num > 57) {
-                rsl = false;
-                break;
+        try {
+            for (char num : check) {
+                if (num < 48 || num > 57) {
+                    rsl = false;
+                    break;
+                }
             }
+        } catch (NumberFormatException nfe) {
+            System.out.println("Please enter validate data again.");
         }
+
         return rsl;
     }
 }

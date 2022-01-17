5f6f551912df69b842711dc39fac14c3f8651b86
Aktsent <accent43@yandex.ru>
2022-01-17 16:11:22 +0300

    SingleTracker

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,45 @@
+package ru.job4j.tracker;
+
+public final class SingleTracker {
+
+    private static SingleTracker instane = null;
+
+    private Tracker tracker = new Tracker();
+
+    private SingleTracker() {
+    }
+
+    public static SingleTracker getInstance() {
+        if (instane == null) {
+            instane = new SingleTracker();
+        }
+        return instane;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return null;
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+
+
+
+}

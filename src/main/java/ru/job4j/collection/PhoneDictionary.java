98c5287bd1f9919e6dcb66c9ac1a8ee98c884afa
Aktsent <accent43@yandex.ru>
2022-02-16 15:19:14 +0300

    PhoneDictionary

--- src/main/java/ru/job4j/collection/PhoneDictionary.java
+++ src/main/java/ru/job4j/collection/PhoneDictionary.java
@@ -11,7 +11,12 @@ public class PhoneDictionary {
 
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
-        result.contains(key);
+        for (Person person : persons) {
+            if (person.getName().contains(key) || person.getSurname().contains(key)
+            || person.getAddress().contains(key) || person.getPhone().contains(key) ) {
+                result.add(person);
+            }
+        }
         return result;
     }
 }

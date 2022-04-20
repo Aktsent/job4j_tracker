47a9350f769e357778938052e81e96e7d4d0edf9
Aktsent <82117481+Aktsent@users.noreply.github.com>
2022-04-20 10:02:20 +0300

    Комбинированный компаратор [#10036]

---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,79 @@
+package ru.job4j.collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+public class JobTest {
+
+    @Test
+    public void whenCompatorByNameAndPrority() {
+        Comparator<Job> cmpNmPr = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNmPr.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenJobDescByPriority() {
+        Job job = new Job("A", 1);
+        Job job1 = new Job("B", 2);
+        Job job2 = new Job("C", 3);
+        List<Job> jobs = Arrays.asList(job, job1, job2);
+        Collections.sort(jobs, new JobDescByPriority());
+        List<Job> expected = Arrays.asList(job2, job1, job);
+        assertThat(jobs, is(expected));
+    }
+
+    @Test
+    public void whenJobAscByPriority() {
+        Job job = new Job("A", 1);
+        Job job1 = new Job("B", 2);
+        Job job2 = new Job("C", 3);
+        List<Job> jobs = Arrays.asList(job2, job, job1);
+        Collections.sort(jobs, new JobAscByPriority());
+        List<Job> expected = Arrays.asList(job, job1, job2);
+        assertThat(jobs, is(expected));
+    }
+
+    @Test
+    public void whenJobAscByName() {
+        Job job = new Job("A", 1);
+        Job job1 = new Job("B", 2);
+        Job job2 = new Job("C", 3);
+        List<Job> jobs = Arrays.asList(job1, job2, job);
+        Collections.sort(jobs, new JobAscByName());
+        List<Job> expected = Arrays.asList(job, job1, job2);
+        assertThat(jobs, is(expected));
+    }
+
+    @Test
+    public void whenJobDescByName() {
+        Job job = new Job("A", 1);
+        Job job1 = new Job("B", 2);
+        Job job2 = new Job("C", 3);
+        List<Job> jobs = Arrays.asList(job, job1, job2);
+        Collections.sort(jobs, new JobDescByName());
+        List<Job> expected = Arrays.asList(job2, job1, job);
+        assertThat(jobs, is(expected));
+    }
+
+    @Test
+    public void whenCompatorByProrityAndName() {
+        Comparator<Job> cmpPrNm = new JobAscByPriority().thenComparing(new JobAscByName());
+        int rsl = cmpPrNm.compare(
+                new Job("A", 1),
+                new Job("B", 2)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+}

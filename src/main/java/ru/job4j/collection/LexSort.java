06aeeab931e03dab2853a3fb7181916acd42fdf8
Aktsent <82117481+Aktsent@users.noreply.github.com>
2022-04-27 10:55:46 +0300

    Сортировка номера [#221284]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -5,17 +5,7 @@ import java.util.Comparator;
 public class LexSort implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-
-        String[] lf = left.split(". ");
-        String[] rg = right.split(". ");
-        for (String l : lf) {
-            for (String r : rg) {
-                int rsl = Integer.compare(Integer.parseInt(l), Integer.parseInt(r));
-                if (rsl != 0) {
-                    return rsl;
-                }
-            }
-        }
-        return left.compareTo(right);
+        return Integer.compare(Integer.parseInt(left.split("\\. ")[0]),
+                Integer.parseInt(right.split("\\. ")[0]));
     }
 }

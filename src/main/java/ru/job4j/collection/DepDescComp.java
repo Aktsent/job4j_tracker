09e765f4d4f9fe7352e59024473c02bf3559ca0c
Aktsent <82117481+Aktsent@users.noreply.github.com>
2022-05-18 14:23:58 +0300

    Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,6 +5,9 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        return o2.compareTo(o1);
+        String[] oo1 = o1.split("/");
+        String[] oo2 = o2.split("/");
+        int res = oo2[0].compareTo(oo1[0]);
+        return res == 0 ? o1.compareTo(o2) : res;
     }
 }
--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -8,7 +8,8 @@ public class Departments {
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                 tmp.add(start + "/" + el);
+                 start += "".equals(start) ? el : "/" + el;
+                 tmp.add(start);
             }
         }
         return new ArrayList<>(tmp);

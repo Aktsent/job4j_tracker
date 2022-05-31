00d68b64e0af0c3fd06172e34280ded7a16e2718
Aktsent <82117481+Aktsent@users.noreply.github.com>
2022-05-31 12:07:38 +0300

    5. Подсчет функции в диапазоне. [#24251]

---
+++ src/main/java/ru/job4j/lambda/Fun.java
@@ -0,0 +1,12 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.*;
+
+public class Fun {
+    public static List<Double> diapason(int start, int end, Function<Double, Double> func) {
+        List<Double> doubl = new ArrayList<>();
+        return doubl;
+    }
+}

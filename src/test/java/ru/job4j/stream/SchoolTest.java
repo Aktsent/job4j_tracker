16ddbf3807f281bc8c2c8a697aa34f1c6df72060
Aktsent <82117481+Aktsent@users.noreply.github.com>
2022-06-07 09:27:00 +0300

    1. Фильтрация учеников [#110058 #309527]

--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -21,7 +21,7 @@ public class SchoolTest {
                 new Student(90, "Surname9")
         );
         School sc = new School();
-        Predicate<Student> pr = score -> score.getScore() >= 70;
+        Predicate<Student> pr = score -> score.getScore() >= 70 && score.getScore() <= 100;
         List<Student> rsl = sc.collect(students, pr);
         List<Student> expected = new ArrayList<>();
         expected.add(new Student(70, "Surname7"));
@@ -39,7 +39,7 @@ public class SchoolTest {
                 new Student(80, "Surname8")
         );
         School sc = new School();
-        Predicate<Student> pr = score -> score.getScore() >= 50;
+        Predicate<Student> pr = score -> score.getScore() >= 50 && score.getScore() <= 70;
         List<Student> rsl = sc.collect(students, pr);
         List<Student> expected = new ArrayList<>();
         expected.add(new Student(50, "Surname5"));

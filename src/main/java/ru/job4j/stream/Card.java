11527e6f5de2b66a04d79f3a6df27dac70d05cdc
Aktsent <82117481+Aktsent@users.noreply.github.com>
2022-06-14 10:17:32 +0300

    8. Перенос методов в Stream API

---
+++ src/main/java/ru/job4j/stream/Card.java
@@ -0,0 +1,28 @@
+package ru.job4j.stream;
+
+import java.util.stream.Stream;
+
+public class Card {
+    private Suit suit;
+    private Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public enum Suit {
+        Diamonds, Hearts, Spades, Clubs
+    }
+
+    public enum Value {
+        V_6, V_7, V_8
+    }
+
+    public static void main(String[] args) {
+        Stream.of(Suit.values())
+                .flatMap(val -> Stream.of(Value.values()))
+                .map(sui -> Suit.values() + " " + Value.values())
+                .forEach(System.out::println);
+    }
+}

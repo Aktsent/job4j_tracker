9cd1f4bbb0008f7fd2e3530cbed90c1e4023bafc
Aktsent <82117481+Aktsent@users.noreply.github.com>
2022-06-29 11:39:46 +0300

    Аттестация

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -6,8 +6,8 @@ import java.util.stream.Stream;
 public class Analyze {
 
     public static double averageScore(Stream<Pupil> stream) {
-        return stream.flatMap()
-                .mapToInt().average().orElse(0D);
+        return stream.flatMap(s -> new Subject(s.getName(), s.getScore)
+                .mapToInt().average((s.getScore + s.getScore) / 2).orElse(0D));
     }
 
     public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
